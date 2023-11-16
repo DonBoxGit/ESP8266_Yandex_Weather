e974246b5fec440a0e36cb127f4866bc7df96b6c
DonBoxGit <donbox@yandex.ru>
2023-11-17 00:46:57 +0300

    Add header comment

--- include/main.h
+++ include/main.h
@@ -1,54 +1,31 @@
 #ifndef _MAIN_H_
-#define _MAIN_H
+#define _MAIN_H_
 
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClientSecure.h>
-#include <ArduinoJson.h>
 #include <EncButton.h>
 
-//#include <time.h>
-//#include "FontsRus/TimesNRCyr6.h"
 #include "weather_icon_elements.h"
 #include "weather_status.h"
 #include "timer_blink.h"
 #include "drawer_functions.h"
+#include "http_request_func.h"
 
+const char* SSID = SSID_ROUTER;
+const char* PASS = PASS_ROUTER;
+
+/* Create an encoder object */
 EncButton encoder(ENC_CLK_PIN, ENC_DT_PIN, ENC_SW_PIN);
-//Button encButton(D0, INPUT_PULLUP, LOW);
+
+/* Create a tft display object  */
+Adafruit_ST7735 tft_display = Adafruit_ST7735(TFT_CS_PIN, TFT_DC_PIN, TFT_RST_PIN);
+
+extern uint16_t weather_id;
+extern int8_t weather_temperature;
+extern struct tm* time_info;
+extern float usd_currency;
+extern float eur_currency;
 
 Blink blinkDots(CLOCK_DOTS_BLINK_PERIOD_MS);
 Timer httpRequestPeriod(WEATHER_REQUEST_PERIOD_MS);
 Timer cbrRequestPeriod(CBR_REQUEST_PERIOD_MS);
-
-static void YandexHttpRequest(void);
-static void CbrHttpRequest(void);
-
-Adafruit_ST7735 tft_display = Adafruit_ST7735(TFT_CS_PIN, TFT_DC_PIN, TFT_RST_PIN);
-StaticJsonDocument<1000> jsonDocument;
-struct tm* time_info;
-static uint16_t weather_id;
-static int8_t weather_temperature;
-
-static float usd_currency;
-static float eur_currency;
-/*------------------------| Get currency value |-----------------------------*/
-#define CHARACTERS_NUMBER 7
-float getCurrency(const String& xml, const String& currency_name) {
-    String sub_string = "<Value>";
-    int currency_position = xml.indexOf(currency_name);
-    int value_position = xml.indexOf(sub_string, currency_position)
-        + sub_string.length();
-    String buff = "";
-    for (int i = value_position; i < value_position + CHARACTERS_NUMBER; i++) {
-        if (xml[i] == ',') {
-            buff += '.';
-            continue;
-        }
-        buff += xml[i];
-    }
-    float curency = buff.toFloat();
-    return curency;
-}
 
 #endif /* _MAIN_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+/*
+  The weather and currency informer.
+           ver. 1.0.0
+                by Yakubovskiy R. N.
+*/
 #include "main.h"
 
 IRAM_ATTR void isr() {
@@ -19,7 +24,7 @@ void setup() {
   tft_display.println ("ST7735 SPI tft initialized");
   //tft.cp437(true);
 
-  WiFi.begin(ssid, pass);
+  WiFi.begin(SSID, PASS);
   tft_display.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     tft_display.print(".");
@@ -113,77 +118,4 @@ void loop() {
     default:
       break;
   }
-}
-
-static void CbrHttpRequest() {
-  WiFiClientSecure wifiClientSecure;
-  HTTPClient httpClient;
-  httpClient.useHTTP10(true);
-  wifiClientSecure.setInsecure();
-  /* Get with UTF-8 code format transformation:
-   https://www.cbr-xml-daily.ru/daily_utf8.xml */
-  httpClient.begin(wifiClientSecure, "https://www.cbr-xml-daily.ru/daily_utf8.xml");
-  Serial.println("Try to get request...");
-  
-  int httpCode = httpClient.GET();
-  Serial.printf("Http code: %d\n", httpCode);
-  String payload = httpClient.getString().substring(2690, 2997);
-  httpClient.end();
-  eur_currency = getCurrency(payload, "EUR");
-  usd_currency = getCurrency(payload, "USD");
-  Serial.print(payload);
-}
-
-static void YandexHttpRequest() {
-    /* Check WiFi connection status */
-    if (WiFi.status() == WL_CONNECTED) {
-      String serverPath = YANDEX_WEATHER_JSON + region_id;
-
-      WiFiClient wifiClient;
-      HTTPClient httpClient;
-      httpClient.begin(wifiClient, serverPath);
-      int httpCode = httpClient.GET();
-
-      if (httpCode > 0) {
-        String response = httpClient.getString();
-        /* Deserialize the JSON document */
-        DeserializationError error = deserializeJson(jsonDocument, response);
-
-        /* Checking the success of parsing */
-        if (error) {
-          String errorStr = error.c_str();
-          Serial.println("Something goin wrong");
-          Serial.println(errorStr);
-        } else {
-        /* Get Unix time as microseconds */
-        time_t unixTime = (jsonDocument["time"]);
-
-        JsonObject clocks = jsonDocument["clocks"][region_id];
-        JsonObject clocks_weather = clocks["weather"];
-
-        /* Get UTC offset as microseconds */
-        unsigned long utc_off_set = clocks["offset"];
-
-        /* Get weather icon name */
-        String weather_icon = clocks_weather["icon"];
-        Serial.print("Weather icon: ");
-        Serial.println(weather_icon);
-        weather_id = calculateWeatherId(weather_icon);
-
-        /* Get temperature */
-        weather_temperature = clocks_weather["temp"];
-       
-        /* Calculate local time from Unix time */
-        unixTime = (unixTime + utc_off_set) / 1000;
-        time_info = localtime(&unixTime);
-        }
-        Serial.printf("Current time - %d:%d\n", time_info->tm_hour, time_info->tm_min);
-      } else {
-        Serial.println("http.GET() == 0");
-      }
-      /* Closing the http connection */
-      httpClient.end();
-    } else {
-      Serial.println("WiFi отключен");
-    }
 }
